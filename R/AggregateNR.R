2ab5052ebb4b8f11c30f575e2bf5899dde50cd4e
nroming <nroming@gmail.com>
2014-12-05 15:06:21 +0100

    calculation of global values in 'AggregateNR' is now optional, but turned on by default

--- R/AggregateNR.R
+++ R/AggregateNR.R
@@ -8,7 +8,7 @@
 #' @return A data frame containing regional aggregates of ag.vars
 #' @export 
 
-AggregateNR <- function(x, ag.vars, region.mapping="reg11"){
+AggregateNR <- function(x, ag.vars, region.mapping="reg11", glob=TRUE){
   require(reshape2)
   # select appropriate subset
   a <- subset(x, item %in% ag.vars)
@@ -34,12 +34,16 @@ AggregateNR <- function(x, ag.vars, region.mapping="reg11"){
   b <- dcast(a, my.formula)
   
   # aggregate values according to region mapping and create global aggregates
-  c <- aggregate(b[ag.vars], b[c("scenario", "year", region.mapping)], FUN=sum, na.rm=TRUE)
+  c <- aggregate(b[ag.vars], b[ ,c("scenario", "year", region.mapping)], FUN=sum, na.rm=TRUE)
   names(c)[names(c)==region.mapping] <- "region" # renaming
-  c.glob <- aggregate(b[ag.vars], b[c("scenario", "year")], FUN=sum, na.rm=TRUE)
-  c.glob$region <- "glob"
-  c.glob <- c.glob[names(c)]
-  c <- rbind(c, c.glob)
+  
+  # optional (default = TRUE) calculation of global values
+  if(glob){
+    c.glob <- aggregate(b[ag.vars], b[c("scenario", "year")], FUN=sum, na.rm=TRUE)
+    c.glob$region <- "glob"
+    c.glob <- c.glob[names(c)]
+    c <- rbind(c, c.glob)
+  }
   
   # create a long data frame
   d <- melt(c, id.vars=c("scenario","year","region"))
